df78db060a5ceafa72022f263c7337a92248bfd4
Vitaly Soroko <v.soroko@tapston.com>
2023-08-17 07:58:35 +0300

    add details screen

--- src/screens/Details/DetailsScreen.tsx
+++ src/screens/Details/DetailsScreen.tsx
@@ -1,13 +1,47 @@
-import {Text} from  'react-native';
-import {SafeAreaView} from "react-native-safe-area-context";
-import {StyleSheet} from "react-native";
-import {theme} from "../../core/theme";
+import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet } from 'react-native';
+import { theme } from '../../core/theme';
+import { useAppSelector } from '../../hooks/redux';
+import { User } from '../../types/types';
 
 export default function DetailsScreen() {
+  const { address, company, email, name, phone, username, website } =
+    useAppSelector(state => state.currentUserSlice.user) as User;
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Text</Text>
+      <Text style={styles.title}>Details</Text>
+      <View style={styles.infoWrapper}>
+        <View style={styles.line}>
+          <Text style={styles.lineTitle}>Name : </Text>
+          <Text style={styles.lineValue}>{name}</Text>
+        </View>
+        <View style={styles.line}>
+          <Text style={styles.lineTitle}>Email : </Text>
+          <Text style={styles.lineValue}>{email}</Text>
+        </View>
+        <View style={styles.line}>
+          <Text style={styles.lineTitle}>User Name : </Text>
+          <Text style={styles.lineValue}>{username}</Text>
+        </View>
+        <View style={styles.line}>
+          <Text style={styles.lineTitle}>Phone : </Text>
+          <Text style={styles.lineValue}>{phone}</Text>
+        </View>
+        <View style={styles.line}>
+          <Text style={styles.lineTitle}>Company : </Text>
+          <Text style={styles.lineValue}>{company.name}</Text>
+        </View>
+        <View style={styles.line}>
+          <Text style={styles.lineTitle}>Address : </Text>
+          <Text style={styles.lineValue}>{address.street}</Text>
+        </View>
+        <View style={styles.line}>
+          <Text style={styles.lineTitle}>Website : </Text>
+          <Text style={styles.lineValue}>{website}</Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
@@ -17,5 +51,28 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: theme.colors.primaryContainer,
     padding: 16,
+  },
+  title: {
+    fontFamily: theme.primaryFont.bold,
+    fontSize: 25,
+    color: theme.colors.black,
+  },
+  infoWrapper: {
+    marginTop: 10,
+  },
+  line: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  lineTitle: {
+    fontFamily: theme.primaryFont.bold,
+    fontSize: 18,
+    marginRight: 10,
+  },
+  lineValue: {
+    fontFamily: theme.primaryFont.medium,
+    fontSize: 16,
+    color: theme.colors.black,
   },
 });
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,37 +1,43 @@
 import {
-    CommonNavigationAction,
-    StackActionType,
-  } from '@react-navigation/native';
-  
-  export type Navigation = {
-    navigate: (scene: string, params?: object) => void;
-    goBack: () => void;
-    replace: (stack: string) => void;
-    getState: () => void;
-    dispatch: (action: CommonNavigationAction | StackActionType) => void;
-  };
-  
+  CommonNavigationAction,
+  StackActionType,
+} from '@react-navigation/native';
 
-  export interface User {
-    id: number;
+export type Navigation = {
+  navigate: (scene: string, params?: object) => void;
+  goBack: () => void;
+  replace: (stack: string) => void;
+  getState: () => void;
+  dispatch: (action: CommonNavigationAction | StackActionType) => void;
+};
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
     name: string;
-    username: string;
-    email: string;
-    address: {
-      street: string;
-      suite: string;
-      city: string;
-      zipcode: string;
-      geo: {
-        lat: string;
-        lng: string;
-      };
-    };
-    phone: string;
-    website: string;
-    company: {
-      name: string;
-      catchPhrase: string;
-      bs: string;
-    };
-  }
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+export interface Icons {
+  users: string;
+  arrowRight: string;
+  search: string;
+  user: string;
+}
